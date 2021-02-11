3e2cc8736515f6e989a28a373f0fae9cb640d6b5
Parker Lazar <plazar@emich.edu>
2021-02-10 20:57:54 -0500

    swipable boxes? maybe?

--- app/home.php
+++ app/home.php
@@ -1,24 +1,48 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/main.css"/>
-    <script src="https://cdn.jsdelivr.net/npm/@shopify/draggable@1.0.0-beta.11/lib/draggable.bundle.js"></script>
-    <script type="text/javascript" src="temp_users.json"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.4/jquery.mobile-1.4.4.min.css"/>
+    <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://code.jquery.com/mobile/1.4.4/jquery.mobile-1.4.4.min.js"></script>
   </head>
 
   <body>
-    <script>
-      var mydata = JSON.parse(data);
-      console.log(mydata);
-    </script>
-    <div class="header">
-        <h1>Header</h1>
-    </div>
-
-    <div class="swipebody">
-    </div>
-
-
-
+      <div id="deck">
+        <div class="buddy"><p style="color: #000;">Text test</p></div>
+        <div class="buddy"><h3 style="color: #000;">Name</h3><p>Biography</p></div>
+      </div>
   </body>
+
+  <script>
+  $(document).ready(function(){
+
+  $(".buddy").on("swiperight",function(){
+    $(this).addClass('rotate-left').delay(700).fadeOut(1);
+    $('.buddy').find('.status').remove();
+
+    if ( $(this).is(':last-child') ) {
+      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+     } else {
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+     }
+  });
+
+ $(".buddy").on("swipeleft",function(){
+  $(this).addClass('rotate-right').delay(700).fadeOut(1);
+  $('.buddy').find('.status').remove();
+
+  if ( $(this).is(':last-child') ) {
+   $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+    alert('OUPS');
+   } else {
+      $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+  }
+});
+
+});
+  </script>
+
 </html>
