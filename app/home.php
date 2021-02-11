c1dceab9089a74dcd893a23c4b6a0248459e3524
Parker Lazar <plazar@emich.edu>
2021-02-10 21:27:11 -0500

    data storage system works, temp code removed

--- app/home.php
+++ app/home.php
@@ -10,39 +10,30 @@
   </head>
 
   <body>
-      <div id="deck">
-        <div class="buddy"><p style="color: #000;">Text test</p></div>
-        <div class="buddy"><h3 style="color: #000;">Name</h3><p>Biography</p></div>
+    <!-- Cards -->
+      <div class="outer-deck">
+        <div class="inner-card"><p style="color: #000;">Swipe test</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
+        <div class="inner-card"><p style="color: #000;"><b>Name</b><br>Biography</p></div>
       </div>
   </body>
-
-  <script>
-  $(document).ready(function(){
-
-  $(".buddy").on("swiperight",function(){
-    $(this).addClass('rotate-left').delay(700).fadeOut(1);
-    $('.buddy').find('.status').remove();
-
-    if ( $(this).is(':last-child') ) {
-      $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-     } else {
-        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-     }
-  });
-
- $(".buddy").on("swipeleft",function(){
-  $(this).addClass('rotate-right').delay(700).fadeOut(1);
-  $('.buddy').find('.status').remove();
-
-  if ( $(this).is(':last-child') ) {
-   $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-    alert('OUPS');
-   } else {
-      $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-  }
-});
-
-});
-  </script>
 
 </html>
