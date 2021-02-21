4801a3b9c9de639aa5a1c961af37f19e603b89dc
StickyKeysEnabled <acrawf26@emich.edu>
2021-02-21 18:41:52 -0500

    Added test case for Login
    Checks the fields for username and password
    Clicks Login button.

--- tests/acceptance/loginTestCest.php
+++ tests/acceptance/loginTestCest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class loginTestCest
 {
@@ -10,14 +10,14 @@ class loginTestCest
     public function tryToTest(AcceptanceTester $I)
     {
     }
-	
+
 	 public function signInSuccessfully(AcceptanceTester $I)
     {
-        $I->amOnPage('http://localhost/cosc481/mainProj/app/index.html');
+        $I->amOnPage('http://localhost/index.html');
         $I->fillField('username','davert');
         $I->fillField('password','qwerty');
         $I->click('Login');
         $I->see('Name19');
-		$I->see('Biography'); 
+        $I->see('Biography');
     }
 }
