adffa08bfa00e4a9d1e0b92d1c4a26ab1d05f185
larry2e <lawrences22e@gmail.com>
2021-02-28 23:48:43 -0500

    added testing file to correct path

---
+++ tests/acceptance/profileTestCest.php
@@ -0,0 +1,34 @@
+<?php
+
+class profileTestCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    //testing login
+    public function logIn(AcceptanceTester $I)
+    {
+      $I->amOnPage('http//localhost/capstone/app/index.php');
+      $I->fillField('userName', 'davert');
+      $I->fillField('password', 'qwerty');
+      $I->click('Login');
+      $I->see('Name19');
+      $I->see('Biography');
+      $I->see('Profile');
+      $I->click('Profile');
+      $I->seeCurrentUrlEquals('http//localhost/capstone/app/profile.php');
+
+    }
+    //testing to see if profile can be viewed
+    public function viewProfile(AcceptanceTester $I){
+      $I->amOnPage('http//localhost/capstone/app/profile.php');
+      $I->see('profilepic');
+      $I->see('bio');
+      $I->see('interests');
+      $I->see('City');
+      $I->see('Edit');
+      $I->click('Edit');
+      $I->seeCurrentUrlEquals('http//localhost/capstone/app/profileedit.php');
+    }
+}
