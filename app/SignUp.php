1bf6bf924fd6f9e80460955c99f2866062d9c781
ParkerAllen2 <parker.allen702+1@gmail.com>
2021-03-10 14:28:14 -0500

    added error handling

--- app/SignUp.php
+++ app/SignUp.php
@@ -9,29 +9,30 @@
 </head>
   <body>
     <div class="container">
-		<form action="../server/signup_fun.php" method="post">
+		<span id='fieldsNotFilled'></span>
+		<form action="../server/signup_fun.php" method="post" onsubmit="return formCheck()">
 			<div class="row">
 				<div class="column">
 					<div class="input_div">
-						<input type="text" name="firstName" placeholder="First name...">
+						<input type="text" class="required" name="firstName" placeholder="First name...">
 					</div>
 				</div>
 				<div class="column">
 					<div class="input_div">
-						<input type="text" name="lastName" placeholder="Last name...">
+						<input type="text" class="required" name="lastName" placeholder="Last name...">
 					</div>
 				</div>
 			</div>
 			<div class="input_div">
-				<input type="text" name="username" placeholder="Username...">
+				<input type="text" class="required" name="username" placeholder="Username...">
 				<span id='confirmUsername'></span>
 			</div>
 			<div class="input_div">
-				<input type="password" name="password" placeholder="Password...">
+				<input type="password" class="required" name="password" placeholder="Password..." onkeyup="checkPwd()">
 			</div>
 			<div class="input_div">
-				<input type="password" name="passwordRepeat" placeholder="Repeat password...">
-				<span id='confirmDifferent'></span>
+				<input type="password" class="required" name="passwordRepeat" placeholder="Repeat password..." onkeyup="checkPwd()">
+				<span id='pwdConfirm'></span>
 			</div>
 			<div class="row">
 				<a href="index.php" class="button">Cancel</a>
@@ -40,4 +41,56 @@
 		</form>
     </div>
   </body>
+  <script>
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	
+	const requiredFields = document.getElementsByClassName('required');
+	const pwdMatchSpan = document.getElementById('pwdConfirm')
+	const requiredSpan = document.getElementById('fieldsNotFilled')
+	
+	if(urlParams.has('error')){
+	
+		if(urlParams.get('error') == 'emptyinput'){
+			
+			console.log(urlParams.get('error'));
+		}
+		if(urlParams.get('error') == 'pwdnotmatch'){
+			pwdMatchSpan.innerHTML = '*not matching';
+			pwdMatchSpan.style.color = 'red';
+		}
+		if(urlParams.get('error') == 'usertaken'){
+			document.getElementById('confirmUsername').innerHTML = '*Username taken';
+			document.getElementById('confirmUsername').style.color = 'red';
+		}
+	}
+	
+	function formCheck(){
+		var noError = checkPwd();
+		for( var i = 0; i < requiredFields.length; i++){
+			if(requiredFields[i].value == ""){
+				requiredFields[i].style.borderColor = 'red';
+				noError = false;
+			}
+		}
+		if(!noError){
+			requiredSpan.innerHTML = '*Please fill in all fields';
+			requiredSpan.style.color = 'red';
+		}
+		return noError;
+	}
+	
+	function checkPwd(){
+		if(requiredFields[3].value == requiredFields[4].value){
+			pwdMatchSpan.innerHTML = '*matching';
+			pwdMatchSpan.style.color = 'green';
+			return true;
+		}
+		else{
+			pwdMatchSpan.innerHTML = '*not matching';
+			pwdMatchSpan.style.color = 'red';
+			return fasle;
+		}
+	}
+  </script>
 </html>
