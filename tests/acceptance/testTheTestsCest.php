ff163d7bad9b212149d4a17f394d498b1c1d4ad3
Ryan Willox <rwillox@emich.edu>
2021-03-13 01:27:22 -0500

    test functions that you are unsure of what they do without putting into other files

---
+++ tests/acceptance/testTheTestsCest.php
@@ -0,0 +1,9 @@
+<?php 
+
+class testTheTestsCest
+{	//for misc testing before actual testing. does not actually need to contain code or run.
+    public function tryToTest(AcceptanceTester $I)
+    {
+		
+    }
+}
