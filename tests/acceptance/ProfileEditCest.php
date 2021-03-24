413796eb278d862a7b55cc8feaadb281ef4229f3
ParkerAllen2 <parker.allen702@gmail.com>
2021-03-24 15:56:58 -0400

    acceptence tests bug fix

--- tests/acceptance/ProfileEditCest.php
+++ tests/acceptance/ProfileEditCest.php
@@ -16,31 +16,14 @@ class profileEditCest
     $I->fillField('password','testPwd');
     $I->click('Login');
     $I->see('Home');
-  }
-
-	public function accessProfile(AcceptanceTester $I)
-  {
-    $I->amOnPage('http://localhost/capstone/app/home.php');
     $I->click('Profile');
 		$I->seeCurrentUrlEquals('/capstone/app/profile.php');
     $I->click('Edit');
 		$I->seeCurrentUrlEquals('/capstone/app/profileedit.php');
-  }
-
-	public function changeInfo(AcceptanceTester $I)
-  {
-    $I->amOnPage('http://localhost/capstone/app/profileedit.php');
-    $I->fillField('desc','x');
-    $I->fillField('addinterests','x');
-    $I->fillField('citytext','x');
-    $I->click('Confirm');
-  }
-
-  public function uploadImage(AcceptanceTester $I)
-  {
-    $I->amOnPage('http://localhost/capstone/app/profileedit.php');
     $I->attachFile('input[name=image]','spider.jpg');
-    $I->click('submit');
+    $I->fillField('desc','testBio');
+    $I->fillField('citytext','testCity');
+    $I->click('Confirm');
   }
 
 	public function storeInDatabase(AcceptanceTester $I)
